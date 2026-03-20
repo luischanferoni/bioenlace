81ea423306327cb23d0a3b26fdbf399efa8eeff9
Luis Chanferoni <luischanferoni@gmail.com>
2026-03-20 18:25:06 -0300

    Create ActionMappingService.php

---
+++ web/common/components/ActionMappingService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace common\components;
+
+/**
+ * Retrocompatibilidad: la implementación vive en {@see Actions\ActionMappingService}.
+ *
+ * @deprecated Usar \common\components\Actions\ActionMappingService
+ */
+class ActionMappingService extends Actions\ActionMappingService
+{
+}

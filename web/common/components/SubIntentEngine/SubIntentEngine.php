dc6e16902aa1aee2d4b4e2893ac7930f32f2f6ce
Luis Chanferoni <luischanferoni@gmail.com>
2026-04-17 09:32:16 -0300

    cambios

--- web/common/components/SubIntentEngine/SubIntentEngine.php
+++ web/common/components/SubIntentEngine/SubIntentEngine.php
@@ -308,6 +308,7 @@ final class SubIntentEngine
             return [
                 'action_id' => $actionId,
                 'client_open' => null,
+                'route' => null,
             ];
         }
         $action = [

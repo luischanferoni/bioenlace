c1c7f9696dab6ca8f43e3b322e3b95a2aa4d7f49
Luis Chanferoni <luischanferoni@gmail.com>
2026-07-27 11:39:50 -0300

    cambios

--- web/frontend/views/layouts/blanco.php
+++ web/frontend/views/layouts/blanco.php
@@ -1,7 +1,56 @@
 <?php
 
-use frontend\assets\CleanAsset;
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-CleanAsset::register($this);
+use yii\helpers\Html;
+use frontend\assets\AppAsset;
 
-echo $content;
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="boxed light theme-color-default">
+<?php $this->beginBody() ?>
+
+<?php if (!Yii::$app->user->isGuest): ?>
+<script>
+window.userPerTabConfig = <?= \yii\helpers\Json::encode(Yii::$app->user->getPerTabSessions()) ?>;
+window.apiAuthToken = <?= json_encode(Yii::$app->session->get('apiJwtToken')) ?>;
+</script>
+<?php endif; ?>
+
+<script>
+window.spaConfig = {
+    baseUrl: '<?= rtrim(Yii::$app->urlManager->createAbsoluteUrl(['/']), '/') ?>',
+    csrfToken: '<?= Yii::$app->request->csrfToken ?>',
+    appVersion: <?= json_encode(Yii::$app->params['spaWebAppVersion'] ?? '1.0.0', JSON_UNESCAPED_UNICODE) ?>
+};
+window.getBioenlaceApiClientHeaders = function (extra) {
+    var ver = (window.spaConfig && window.spaConfig.appVersion) ? String(window.spaConfig.appVersion) : '1.0.0';
+    var base = {
+        'X-App-Client': 'web-frontend',
+        'X-App-Version': ver,
+        'X-Client': 'web'
+    };
+    if (window.apiAuthToken) {
+        base['Authorization'] = 'Bearer ' + window.apiAuthToken;
+    }
+    return Object.assign(base, extra || {});
+};
+</script>
+
+<?= $content ?>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>

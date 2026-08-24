3800548a5859e743b9866e040803600ce9fbadfe
Luis Chanferoni <luischanferoni@gmail.com>
2026-08-24 11:04:39 -0300

    cambios

--- web/common/components/Platform/Infra/Log/ResilientDbTarget.php
+++ web/common/components/Platform/Infra/Log/ResilientDbTarget.php
@@ -6,7 +6,6 @@ use Yii;
 use yii\helpers\FileHelper;
 use yii\log\DbTarget;
 use yii\log\FileTarget;
-use yii\log\Logger;
 
 /**
  * DbTarget que no amplifica "MySQL server has gone away": si falla el INSERT en log, escribe en archivo.
@@ -33,7 +32,7 @@ class ResilientDbTarget extends DbTarget
         $fallback = new FileTarget([
             'logFile' => $dir . '/db-target-fallback.log',
             'exportInterval' => 1,
-            'levels' => [Logger::LEVEL_ERROR, Logger::LEVEL_WARNING, Logger::LEVEL_INFO],
+            'levels' => ['error', 'warning', 'info'],
         ]);
         $fallback->messages = $this->messages;
         $fallback->export();
